3f845a225227ba7577af0d314b3961b98321f3c4
Arne de Laat <arne@delaat.net>
2021-12-13 07:12:48 +0100

    Implement a solution for day 3 part 2

--- aoc2021/day3.py
+++ aoc2021/day3.py
@@ -25,7 +25,34 @@ def part1(diagnostics):
 
 
 def part2(diagnostics):
-    ...
+    diagnostics.sort()
+
+    n_bits = len(diagnostics[0])
+
+    oxygen_candidates = diagnostics
+    co2_candidates = diagnostics
+
+    for bit in range(n_bits):
+        oxygen_groups = {key: list(group) for key, group in groupby(oxygen_candidates, key=itemgetter(bit))}
+        if len(oxygen_groups) == 1:
+            oxygen_candidates = list(oxygen_groups.values())[0]
+        elif len(oxygen_groups['0']) > len(oxygen_groups['1']):
+            oxygen_candidates = oxygen_groups['0']
+        else:
+            oxygen_candidates = oxygen_groups['1']
+
+        co2_groups = {key: list(group) for key, group in groupby(co2_candidates, key=itemgetter(bit))}
+        if len(co2_groups) == 1:
+            co2_candidates = list(co2_groups.values())[0]
+        elif len(co2_groups['1']) < len(co2_groups['0']):
+            co2_candidates = co2_groups['1']
+        else:
+            co2_candidates = co2_groups['0']
+
+    oxygen = oxygen_candidates[0]
+    co2 = co2_candidates[0]
+
+    return int(oxygen, 2) * int(co2, 2)
 
 
 if __name__ == '__main__':
