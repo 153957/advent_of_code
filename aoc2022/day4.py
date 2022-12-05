2a5266ace832cbc6e30a4335c143ae21f928cb3f
Arne de Laat <arne@delaat.net>
2022-12-05 21:38:05 +0100

    Remove unnecessary character in variable names

--- aoc2022/day4.py
+++ aoc2022/day4.py
@@ -7,11 +7,11 @@ def part1(assignments):
     contained_pairs = 0
     for assignment in assignments:
         elf1, _, elf2 = assignment.partition(',')
-        start11, _, end11 = elf1.partition('-')
-        start21, _, end21 = elf2.partition('-')
+        start1, _, end1 = elf1.partition('-')
+        start2, _, end2 = elf2.partition('-')
         if (
-            int(start11) <= int(start21) and int(end11) >= int(end21)
-            or int(start11) >= int(start21) and int(end11) <= int(end21)
+            int(start1) <= int(start2) and int(end1) >= int(end2)
+            or int(start1) >= int(start2) and int(end1) <= int(end2)
         ):
             contained_pairs += 1
     return contained_pairs
@@ -20,13 +20,13 @@ def part2(assignments):
     overlapping_pairs = 0
     for assignment in assignments:
         elf1, _, elf2 = assignment.partition(',')
-        start11, _, end11 = elf1.partition('-')
-        start21, _, end21 = elf2.partition('-')
+        start1, _, end1 = elf1.partition('-')
+        start2, _, end2 = elf2.partition('-')
         if (
-            int(start11) <= int(start21) <= int(end11)
-            or int(start11) <= int(end21) <= int(end11)
-            or int(start21) <= int(start11) <= int(end21)
-            or int(start21) <= int(end11) <= int(end21)
+            int(start1) <= int(start2) <= int(end1)
+            or int(start1) <= int(end2) <= int(end1)
+            or int(start2) <= int(start1) <= int(end2)
+            or int(start2) <= int(end1) <= int(end2)
         ):
             overlapping_pairs += 1
     return overlapping_pairs
